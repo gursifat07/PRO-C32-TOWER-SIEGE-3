1eb63c57b8ef53b3970432b7032c051608a72a32
gursifat07 <74097133+gursifat07@users.noreply.github.com>
2021-01-27 21:30:42 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,42 @@
+class Box {
+    constructor(x, y, width, height) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.5,
+          'density':1.0
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.visibility=255
+      
+      World.add(world, this.body);
+    }
+    display(){
+     
+      if(this.body.speed<7){
+        this.pos=this.body.position;
+       push();
+       translate(this.pos.x,this.pos.y)
+       rectMode(CENTER);
+       fill(62, 223, 207);
+       rect(0, 0, this.width, this.height);
+       pop();
+   }
+   else{
+       World.remove(world,this.body);
+       push();
+       tint(255,this.visibility);
+       this.visibility=this.visibility-5;
+       pop();
+   }
+  
+ }
+  score() {
+      if(this.visibility < 0 && this.visibility > -105){
+          score = score + 1;
+          console.log("hi")
+
+      }
+  }
+}
